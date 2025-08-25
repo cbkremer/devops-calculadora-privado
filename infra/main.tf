46ab9c20699cccb07b7f0dd0ab7c016db138be47
Carlos Kremer <cbpontokremer@gmail.com>
2025-08-25 20:50:49 -0300

    test

--- infra/main.tf
+++ infra/main.tf
@@ -6,17 +6,27 @@ resource "random_id" "suffix" {
   byte_length = 4
 }
 
-resource "aws_s3_bucket" "app_bucket" {
-  bucket = "sandbox-devops-cbkremer-${random_id.suffix.hex}"
+# Disable block public access
+resource "aws_s3_bucket_public_access_block" "app_bucket" {
+  bucket = aws_s3_bucket.app_bucket.id
 
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
-  }
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.app_bucket.id
+# Bucket website configuration
+resource "aws_s3_bucket_website_configuration" "app_website" {
+  bucket = aws_s3_bucket.app_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
 }
 
 # Bucket policy
@@ -34,4 +44,8 @@ resource "aws_s3_bucket_policy" "app_bucket_policy" {
       }
     ]
   })
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.app_bucket.id
 }
